28fadc5b8099b556cdee1e1d58495390297e3949
Adebayour66265 <adebayour66265@gmail.com>
2023-11-23 10:53:26 +0100

    refactors API key

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const swaggerUI = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const swaggerOptions = {
-    explorer: true,
+  explorer: true,
 };
-require('dotenv').config()
+require("dotenv").config();
 
-
-const connectDB = require('./config/db');
-
+const connectDB = require("./config/db");
 
 const app = express();
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.get('/', (req, res) => {
-    res.json("Well come");
-    res.send('Hello World!');
-})
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument, swaggerOptions));
-app.use('/admin', require('./routes/admin'));
-app.use('/driver', require('./routes/admin'));
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument, swaggerOptions)
+);
+app.use("/admin", require("./routes/admin"));
+app.use("/driver", require("./routes/admin"));
 connectDB();
 app.listen(process.env.port || 5000, () => {
-    console.log('Listening on port 8081');
-})
+  console.log("Listening on port 8081");
+});
